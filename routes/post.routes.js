d3456ebe2475d780d83594516079f60b7be370b8
William Lebois <william.lebois@gmail.com>
2022-12-26 13:54:24 +0100

    last commit

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,9 +10,6 @@ router.delete('/:id', postController.deletePost);  // Pour supprimer un post
 router.patch('/like-post/:id', postController.likePost);  // Pour liker un post
 router.patch('/unlike-post/:id', postController.unlikePost);  // Pour unliker un post
 
-// comments
-router.patch('/comment-post/:id', postController.commentPost);  // Route pour laisser un commentaire
-router.patch('/edit-comment-post/:id', postController.editCommentPost);  // Route pour editer un commentaire
-router.patch('/delete-comment-post/:id', postController.deleteCommentPost);  // Route pour supprimer un commentaire
+
 
 module.exports = router;
